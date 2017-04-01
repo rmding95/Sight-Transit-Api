2e603e31e3b0be12f497a0d05f2f9f0f0dae12c9
Richard Ding <richardding@D-173-250-247-194.dhcp4.washington.edu>
2017-04-01 16:19:08 -0700

    setup

---
+++ server.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+
+var router = express.Router();              // get an instance of the express Router
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });   
+});
+
+// more routes for our API will happen here
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(8080);
+console.log('Magic happens on port ' + 8080);
