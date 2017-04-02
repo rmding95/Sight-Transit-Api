b5ad7cf1c791b419f3dcb475c70f159fd585e6b9
Richard Ding <richardding@D-173-250-247-194.dhcp4.washington.edu>
2017-04-01 18:15:15 -0700

    trying out api stuff

--- proxy.js
+++ proxy.js
@@ -1,2 +1,15 @@
 'use strict';
 
+var request = require('request');
+var key = 'ed53d6fe-60fc-4fa1-9731-22bea8a64bd0';
+var baseUrl = 'http://api.pugetsound.onebusaway.org/api/where/';
+
+var proxy = module.exports = {};
+
+proxy.getAgencies = function(callback) {
+
+    var call = request(baseUrl + 'agencies-with-coverage.json?key=' + key, function(error, response, body) {
+        callback(response);
+    });
+
+};
--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 'use strict';
 
 var express = require('express');
+var proxy = require('./proxy.js');
 var app = express();
 
 var router = express.Router();              // get an instance of the express Router
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
+    var response = proxy.getAgencies(function(data) {
+        res.json({ message: data });   
+    });
+
 });
+
+router.route('/route').get(function(req, res) {
+    res.json({ message: 'data' });
+});
+    
+
+
 
 // more routes for our API will happen here
 
